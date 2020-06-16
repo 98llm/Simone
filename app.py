f0a349942517bd1084b343dc9ea52267f8623e8c
Leandro <limamonteiro2010@hotmail.com>
2020-06-16 16:01:23 -0300

    a

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
