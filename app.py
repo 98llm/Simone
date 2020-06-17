800c20351a3bd6ce10116e889135e6bd695fed82
Leandro <limamonteiro2010@hotmail.com>
2020-06-17 01:17:12 -0300

    a

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    title = {'titulo': 'Página principal',}
+    return render_template('index.html', **title)
 
 
 if __name__ == '__main__':
