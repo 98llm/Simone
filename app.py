ff81d22e4ed002175610603b73804e65f664ee17
Leandro <limamonteiro2010@hotmail.com>
2020-06-19 16:25:02 -0300

    a

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = {'titulo': 'Página principal',}
+    title = {'titulo': 'Página principal'}
     return render_template('index.html', **title)
 
 
